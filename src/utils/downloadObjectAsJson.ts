21c639b7d30785593129482ddeea54f90949570d
pauloricca <pauloricca@gmail.com>
2024-01-28 13:41:33 +0000

    save us attempt, not opening dialog

--- src/utils/downloadObjectAsJson.ts
+++ src/utils/downloadObjectAsJson.ts
@@ -1,11 +1,15 @@
+import { saveAs } from 'file-saver';
+
 const downloadObjectAsJson = (exportObj: any, exportName: string) => {
-	var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(exportObj, null, 2));
-	var downloadAnchorNode = document.createElement('a');
-	downloadAnchorNode.setAttribute("href",     dataStr);
-	downloadAnchorNode.setAttribute("download", exportName + ".json");
-	document.body.appendChild(downloadAnchorNode); // required for firefox
-	downloadAnchorNode.click();
-	downloadAnchorNode.remove();
+	// const dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(exportObj, null, 2));
+	// const downloadAnchorNode = document.createElement('a');
+	// downloadAnchorNode.setAttribute("href",     dataStr);
+	// downloadAnchorNode.setAttribute("download", exportName + ".json");
+	// document.body.appendChild(downloadAnchorNode); // required for firefox
+	// downloadAnchorNode.click();
+	// downloadAnchorNode.remove();
+	var blob = new Blob([encodeURIComponent(JSON.stringify(exportObj, null, 2))], {type: "data:text/json;charset=utf-8"});
+	saveAs(blob, exportName + ".json");
 }
 
 export default downloadObjectAsJson;
