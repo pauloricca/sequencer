bc9402157fb3d1ad1ef30e3afb3a9a672a2deef0
pauloricca <pauloricca@gmail.com>
2024-05-23 01:57:44 +0100

    fix bug with detecting other nodes as input in focus

--- src/components/ShortcutController/ShortcutController.utils.ts
+++ src/components/ShortcutController/ShortcutController.utils.ts
@@ -1,4 +1,4 @@
-export const hasInputInFocus = () => document.activeElement !== document.body;
+export const hasInputInFocus = () => document.activeElement?.tagName.toLowerCase() === 'input';
 
 /**
  * Given a number of decimal places (undefined, 0, 1, 2, 3), returns the increment step (1, 1, 0.1, 0.01, 0.001)
